acd352db96b72fb2c09ac3c9717d588fa34be65d
David Kraeutmann <kane@kane.cx>
2015-03-21 02:27:36 +0100

    Fix run scripts and add -static to Makefile

--- run_serena.sh
+++ run_serena.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env zsh
  
 ### Job name
-#BSUB -J OPENMPJOB
+#BSUB -J SERENAJOB
  
 ### File / path where STDOUT & STDERR will be written
 ###    %J is the job ID, %I is the array ID
-#BSUB -o OPENMPJOB.%J.%I
+#BSUB -o SERENAJOB.%J.%I
  
 ### Request the time you need for execution in minutes
 ### The format for the parameter is: [hour:]minute,
@@ -15,10 +15,10 @@
 #BSUB -P lect0005
  
 ### Request memory you need for your job in TOTAL in MB
-#BSUB -M 1024
+#BSUB -M 8192
  
 ### Request the number of compute slots you want to use
-#BSUB -n 12
+#BSUB -n 64
  
 ### Use esub for OpenMP/shared memeory jobs
 #BSUB -a openmp
@@ -28,4 +28,5 @@ cd /home/dk406646/swp-hpc/src
  
 ### Execute your application
 
-CG_MAX_ITER=6000 OMP_PLACES=cores OMP_PROC_BIND=spread  ./cg.exe /home/lect0005/matrix/G3_circuit.mtx
+CG_MAX_ITER=6000 OMP_PLACES=cores OMP_PROC_BIND=spread  ./cg.exe /home/lect0005/matrix/Serena.mtx
+
