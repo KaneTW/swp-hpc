43c8d24f7642f53faff5629cc6eed4c12a1573a7
David Kraeutmann <kane@kane.cx>
2015-04-14 16:24:05 +0200

    Missing solver.h

--- src/solver.h
+++ src/solver.h
@@ -20,9 +20,10 @@
 	extern "C" {
 #endif
 	void vectorDot(const floatType* a, const floatType* b, const int n, floatType* ab);
-	void axpy(const floatType a, const floatType* x, const int n, floatType* y);
-	void xpay(const floatType* x, const floatType a, const int n, floatType* y);
-	void matvec(const int n, const int nnz, const int maxNNZ, const floatType* data, const int* indices, const int* length, const floatType* x, floatType* y);
+        void vectorSquare(const floatType* x, const int n, floatType* aa);
+	__global__ void axpy(const floatType a, const floatType* __restrict__ x, const int n, floatType* __restrict__ y);
+	__global__ void xpay(const floatType* __restrict__ x, const floatType a, const int n, floatType* __restrict__ y);
+	__global__ void matvec(const int n, const int nnz, const int maxNNZ, const floatType* __restrict__ data, const int* __restrict__ indices, const int* __restrict__ length, const floatType* __restrict__ x, floatType* __restrict__ y);
 	void nrm2(const floatType* x, const int n, floatType* nrm);
 	void cg(const int n, const int nnz, const int maxNNZ, const floatType* data, const int* indices, const int* length, const floatType* b, floatType* x, struct SolverConfig* sc);
 #ifdef __cplusplus
